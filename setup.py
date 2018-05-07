eea713c9243c6c2129ca1512f33b234f43f2b4e0
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-05-07 17:58:28 +0300

    packages manual

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from distutils.core import setup
 setup(name='exonum',
       description='Python Exonum Client',
       url='https://github.com/exonum/python-client/',
-      py_modules=['exonum'],
-      packages=find_packages(),
+      packages=['exonum'],
       python_requires='>=3.5',
       install_requires=[
           'nanotime',
