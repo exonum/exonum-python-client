1553c4a136125ec379a49d78425a253ddaeed51d
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-05-07 17:53:13 +0300

    package dir

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='exonum',
       description='Python Exonum Client',
       url='https://github.com/exonum/python-client/',
       py_modules=['exonum'],
+      package_dir={'exonum': 'exonum'},
       python_requires='>=3.5',
       install_requires=[
           'nanotime',
