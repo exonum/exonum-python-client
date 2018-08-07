21f718dc2d464d01e05003b62c6bf8f5b39a22c1
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2018-08-07 12:43:29 +0300

    Added example how to use tx hash

--- work.py
+++ work.py
@@ -1,8 +1,8 @@
-from uuid import uuid4
-
+import json
 import exonum.transactions as tx
 import exonum.datatypes as exonum
 
+from uuid import uuid4
 from pysodium import crypto_sign_keypair
 
 from importlib import reload
@@ -24,21 +24,18 @@ transactions = tx.transactions(service_id=521)
 # a.tx(secret_key)
 
 public_key = bytes.fromhex(
-    "0f17189c062e7f3fbb47a21834d41e4d5c5388dd7db38c4de1ce732971a38ef9"
-)
+    "0f17189c062e7f3fbb47a21834d41e4d5c5388dd7db38c4de1ce732971a38ef9")
 secret_key = bytes.fromhex(
-    "5520c351b7760aedeef32687918eb2587ab515e4ae0eeef271a0f0a99f1df3710f17189c062e7f3fbb47a21834d41e4d5c5388dd7db38c4de1ce732971a38ef9"
-)
+    "5520c351b7760aedeef32687918eb2587ab515e4ae0eeef271a0f0a99f1df3710f17189c062e7f3fbb47a21834d41e4d5c5388dd7db38c4de1ce732971a38ef9")
 
 
 @transactions
 class CreateUser(metaclass=exonum.EncodingStruct):
-    public_key = exonum.PublicKey
-    name = exonum.Str
+    public_key = exonum.PublicKey()
+    name = exonum.Str()
 
 
 a = CreateUser(public_key=public_key, name="Me")
 
-import json
-
 print(json.dumps(a.tx(secret_key), indent=2))
+print('tx hash:', a.hash(secret_key))
