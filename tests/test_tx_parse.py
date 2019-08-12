daa6d22d067667031ca83fc245e635072befc005
Igor Aleksanov <popzxc@yandex.ru>
2019-08-12 19:47:10 +0300

    Make tests better

--- tests/test_tx_parse.py
+++ tests/test_tx_parse.py
@@ -15,7 +15,8 @@ SERVICE_ID = 130
 MESSAGE_ID = 1
 
 class TestTxParse(unittest.TestCase):
-    def test_tx_success_parse(self):
+    @classmethod
+    def setUpClass(cls):
         # Add folder with pre-compiled protobuf messages to the path (so it can be imported)
         sys.path.append(os.path.abspath('tests/proto_dir'))
 
@@ -35,60 +36,43 @@ class TestTxParse(unittest.TestCase):
         create_wallet_alice_tx = cryptocurrency_message_generator.create_message('CreateWallet', create_wallet_alice)
         create_wallet_alice_tx.sign(keys)
 
-        # Parse message
-        parsed_message = ExonumMessage.from_hex(create_wallet_alice_tx.raw.hex(), cryptocurrency_service_name, 'CreateWallet')
+        cls.keys = keys
+        cls.exonum_message = create_wallet_alice_tx
+        cls.cryptocurrency_service_name = cryptocurrency_service_name
 
-        self.assertEqual(parsed_message.get_author(), keys[0])
-        self.assertEqual(parsed_message.service_id, create_wallet_alice_tx.service_id)
-        self.assertEqual(parsed_message.message_id, create_wallet_alice_tx.message_id)
-        self.assertEqual(parsed_message.hash(), create_wallet_alice_tx.hash())
+    @classmethod
+    def tearDownClass(self):
+        # Remove protobuf directory from the path.
+        sys.path.remove(os.path.abspath('tests/proto_dir'))
+
+
+    def test_tx_success_parse(self):
+        exonum_message = self.exonum_message
+        service_name = self.cryptocurrency_service_name
+        
+        # Parse message
+        parsed_message = ExonumMessage.from_hex(exonum_message.raw.hex(), service_name, 'CreateWallet')
+
+        self.assertEqual(parsed_message.get_author(), TestTxParse.keys[0])
+        self.assertEqual(parsed_message.service_id, exonum_message.service_id)
+        self.assertEqual(parsed_message.message_id, exonum_message.message_id)
+        self.assertEqual(parsed_message.hash(), exonum_message.hash())
 
     def test_tx_fail_parse(self):
-        # Add folder with pre-compiled protobuf messages to the path (so it can be imported)
-        sys.path.append(os.path.abspath('tests/proto_dir'))
-
-        # Gen init data
-        keys = crypto_sign_keypair()
-
-        # Prepare original message
-        cryptocurrency_service_name = 'exonum-cryptocurrency-advanced:0.11.0'
-        cryptocurrency_module = ModuleManager.import_service_module(cryptocurrency_service_name, 'service')
-
-        cryptocurrency_message_generator = MessageGenerator(1024, cryptocurrency_service_name)
-
-        create_wallet_alice = cryptocurrency_module.CreateWallet()
-        create_wallet_alice.name = 'Alice'
-
-        # Create original message
-        create_wallet_alice_tx = cryptocurrency_message_generator.create_message('CreateWallet', create_wallet_alice)
-        create_wallet_alice_tx.sign(keys)
+        exonum_message = self.exonum_message
+        service_name = self.cryptocurrency_service_name
 
         # Parse message
-        corrupted_message = '1a' + create_wallet_alice_tx.raw.hex()
-        parsed_message = ExonumMessage.from_hex(corrupted_message, cryptocurrency_service_name, 'CreateWallet')
+        corrupted_message = '1a' + exonum_message.raw.hex()
+        parsed_message = ExonumMessage.from_hex(corrupted_message, service_name, 'CreateWallet')
 
         self.assertIsNone(parsed_message)
 
     def test_tx_validation(self):
-        # Add folder with pre-compiled protobuf messages to the path (so it can be imported)
-        sys.path.append(os.path.abspath('tests/proto_dir'))
+        exonum_message = self.exonum_message
 
         # Gen init data
-        keys = crypto_sign_keypair()
         fake_keys = crypto_sign_keypair()
-
-        # Prepare original message
-        cryptocurrency_service_name = 'exonum-cryptocurrency-advanced:0.11.0'
-        cryptocurrency_module = ModuleManager.import_service_module(cryptocurrency_service_name, 'service')
-
-        cryptocurrency_message_generator = MessageGenerator(1024, cryptocurrency_service_name)
-
-        create_wallet_alice = cryptocurrency_module.CreateWallet()
-        create_wallet_alice.name = 'Alice'
-
-        # Create original message
-        exonum_message = cryptocurrency_message_generator.create_message('CreateWallet', create_wallet_alice)
-        exonum_message.sign(keys)
 
         # Checks that origin message validates right
         self.assertTrue(exonum_message.validate())
