fce83ee4e00eee07797a0459452f62b61d81aea3
Igor Aleksanov <popzxc@yandex.ru>
2019-08-12 21:45:34 +0300

    Add more tests for exonum client

--- tests/test_exonum_client.py
+++ tests/test_exonum_client.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
 import sys
+import os
 
 from exonum.client import ExonumClient
 from exonum.module_manager import ModuleManager
@@ -13,10 +14,10 @@ EXONUM_URL_BASE = '{}://{}:{}/'
 
 SYSTEM_ENDPOINT_POSTFIX = 'api/system/v1/{}'
 
-def proto_sources_main_response():
+def proto_sources_response(service):
     from requests.models import Response
 
-    with open('tests/api_responses/proto_sources_main.json') as file:
+    with open('tests/api_responses/proto_sources_{}.json'.format(service)) as file:
         content = file.read()
 
         response = Response()
@@ -36,24 +37,41 @@ def mock_requests_get(url, params=None):
 
 
     responses = {
-        # Proto sources endpoint
-        (proto_sources_endpoint, None): proto_sources_main_response()
+        # Proto sources endpoints
+
+        # Proto sources without params (main sources)
+        (proto_sources_endpoint, "None"): proto_sources_response('main'),
+        # Proto sources for supervisor service
+        (proto_sources_endpoint, "{'artifact': '0:exonum-supervisor:0.11.0'}"): proto_sources_response('supervisor')
 
     }
 
-    return responses[(url, params)]
+    return responses[(url, str(params))]
 
 class TestExonumClient(unittest.TestCase):
     # This test case replaces get function from exonum client with the mock one.
     # Thus testing of HTTP interacting could be done without actual exonum client.
 
-    @classmethod
-    def setUpClass(cls):
-        # Unload any previously loaded `exonum_main` modules from test_tx_parse
+    def setUp(cls):
+        # Unload any previously loaded `exonum_main` modules from other tests
         loaded_modules = list(sys.modules.keys())
         for module in loaded_modules:
             if module.startswith('exonum_modules'):
                 del sys.modules[module]
+
+    def test_client_creates_temp_folder(self):
+        # Test that proto directory is created and added to sys.path
+
+        with ExonumClient(hostname=EXONUM_IP, public_api_port=EXONUM_PUBLIC_PORT,
+                          private_api_port=EXONUM_PRIVATE_PORT) as client:
+            self.assertTrue(os.path.isdir(client.proto_dir))
+            self.assertTrue(os.path.exists(client.proto_dir))
+            self.assertTrue(client.proto_dir in sys.path)
+
+        # Test that everything is cleaned up after use
+        self.assertFalse(os.path.isdir(client.proto_dir))
+        self.assertFalse(os.path.exists(client.proto_dir))
+        self.assertFalse(client.proto_dir in sys.path)
 
     @patch('exonum.client.get', new=mock_requests_get)
     def test_main_sources_download(self):
@@ -62,3 +80,14 @@ class TestExonumClient(unittest.TestCase):
             client.load_main_proto_files()
 
             runtime_mod = ModuleManager.import_main_module('runtime')
+
+    @patch('exonum.client.get', new=mock_requests_get)
+    def test_service_sources_download(self):
+        with ExonumClient(hostname=EXONUM_IP, public_api_port=EXONUM_PUBLIC_PORT,
+                          private_api_port=EXONUM_PRIVATE_PORT) as client:
+            client.load_main_proto_files()
+            client.load_service_proto_files(0, 'exonum-supervisor:0.11.0')
+
+            service_module = ModuleManager.import_service_module('exonum-supervisor:0.11.0', 'service')
+
+    # TODO add more tests
