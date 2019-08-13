a9fb9103c512fc0500a4cf6c648f78100a80052b
Igor Aleksanov <popzxc@yandex.ru>
2019-08-13 07:22:03 +0300

    Move usage example to the work.py

--- work.py
+++ work.py
@@ -1,50 +1,122 @@
-import proto.cryptocurrency_pb2
-from exonum.client import ExonumClient
-from exonum.message import MessageGenerator, gen_keypair, encode
-
-
 def event_handler(data):
     print(data)
 
+def example_run():
+    # Example of usage.
 
-def main():
-    try:
-        # Creation client, subscriber and message generator
-        client = ExonumClient("cryptocurrency", "192.168.1.177", 9081)
+    from .module_manager import ModuleManager
+    from .message import MessageGenerator, gen_keypair
+    import codecs
+    import json
+    import time
+
+    with ExonumClient(hostname='127.0.0.1', public_api_port=8080, private_api_port=8081) as client:
+        client.load_main_proto_files()
+        client.load_service_proto_files(0, 'exonum-supervisor:0.11.0')
+
+        main_module = ModuleManager.import_main_module('consensus')
+
+        service_module = ModuleManager.import_service_module('exonum-supervisor:0.11.0', 'service')
+        
+        print(client.available_services().json())
+
+        # Create subscriber
+        # TODO FIXME subscriber is not created for some reason (Handshake Status 400)
+
         subscriber = client.create_subscriber()
-        msg_generator = MessageGenerator(proto.cryptocurrency_pb2, 128)
 
-        # Creation Alice's wallet
-        alice_keys = gen_keypair()
-        alice_msg = msg_generator.create_message("CreateWallet", name="Alice")
-        client.send_transaction(alice_msg.sign(alice_keys))
-        subscriber.wait_for_new_block()
-        print(client.get_tx_info(alice_msg.hash()))
+        # Deploy cryptocurrency service
 
-        # Creation Bob's wallet
-        bob_keys = gen_keypair()
-        bob_msg = msg_generator.create_message("CreateWallet", name="Bob")
-        client.send_transaction(bob_msg.sign(bob_keys))
-        subscriber.wait_for_new_block()
-        print(client.get_tx_info(bob_msg.hash()))
+        cryptocurrency_service_name = 'exonum-cryptocurrency-advanced:0.11.0'
+        deploy_request = service_module.DeployRequest()
+        deploy_request.artifact.runtime_id = 0
+        deploy_request.artifact.name = cryptocurrency_service_name
+        deploy_request.deadline_height = 1000000
 
-        # Transfer funds from Bob's wallet to Alice's
-        transfer_msg = msg_generator.create_message("Transfer", amount=10, seed=12345)
-        transfer_msg.data.to.data = alice_keys[0]
-        print(client.send_transaction(transfer_msg.sign(bob_keys)))
-        subscriber.wait_for_new_block()
-        print(client.get_tx_info(transfer_msg.hash()))
+        hex_request = codecs.encode(deploy_request.SerializeToString(), "hex").decode("utf-8")
 
-        print(client.get_service("wallets/info?pub_key=" + encode(alice_keys[0])))
+        request_json = json.dumps(hex_request)
+        supervisor_endpoint = "http://127.0.0.1:8081/api/services/supervisor/{}"
+        deploy_endpoint = supervisor_endpoint.format('deploy-artifact')
+
+        response = requests.post(deploy_endpoint, request_json, headers={"content-type": "application/json"})
+
+        print('-------')
+        print(response.json())
+
+        # Wait for new blocks
+        # TODO Use subscriber
+
+        time.sleep(2)
+
+        # Start cryptocurrency service
+
+        start_request = service_module.StartService()
+        start_request.artifact.runtime_id = 0
+        start_request.artifact.name = cryptocurrency_service_name
+        start_request.name = 'XNM'
+        start_request.deadline_height = 1000000
+
+        hex_request = codecs.encode(start_request.SerializeToString(), "hex").decode("utf-8")
+
+        request_json = json.dumps(hex_request)
+        start_endpoint = supervisor_endpoint.format('start-service')
+
+        response = requests.post(start_endpoint, request_json, headers={"content-type": "application/json"})
+
+        # Wait for new blocks
+        # TODO Use subscriber
+
+        time.sleep(2)
+
+        print('-----')
+        print(client.available_services().json())
+
+        # Work with cryptocurrency
+
+        keys = gen_keypair()
+
+        client.load_service_proto_files(0, cryptocurrency_service_name)
+
+        cryptocurrency_module = ModuleManager.import_service_module(cryptocurrency_service_name, 'service')
+
+        cryptocurrency_message_generator = MessageGenerator(1024, cryptocurrency_service_name)
+
+        # Create wallet for Alice
+
+        create_wallet_alice = cryptocurrency_module.CreateWallet()
+        create_wallet_alice.name = 'Alice'
+        create_wallet_alice_tx = cryptocurrency_message_generator.create_message('CreateWallet', create_wallet_alice)
+        create_wallet_alice_tx.sign(keys)
+
+        # Create wallet for Bob
+
+        create_wallet_bob = cryptocurrency_module.CreateWallet()
+        create_wallet_bob.name = 'Bob'
+        create_wallet_bob_tx = cryptocurrency_message_generator.create_message('CreateWallet', create_wallet_bob)
+        create_wallet_bob_tx.sign(keys)
+
+        responses = client.send_transactions([create_wallet_alice_tx, create_wallet_bob_tx])
+
+        # Wait for new blocks
+        # TODO Use subscriber
+
+        time.sleep(2)
+
+        # Show transactions statuses
+
+        for response in responses:
+            res = client.get_tx_info(response.json()['tx_hash'])
+            print(res)
+            print(res.json())
+
+        # Some additional info.
         print(client.health_info())
         print(client.mempool())
         print(client.user_agent())
 
-        subscriber.stop()
-
-        print("Bye, demo is over...")
-    except KeyboardInterrupt:
-        subscriber.stop()
+def main():
+    example_run()
 
 
 if __name__ == "__main__":
