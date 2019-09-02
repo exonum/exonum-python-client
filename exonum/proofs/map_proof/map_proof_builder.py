e94e46bc699d227dc38842ce286b8704d9c1e0fd
Igor Aleksanov <popzxc@yandex.ru>
2019-09-02 14:16:06 +0300

    Document MapProofBuilder

--- exonum/proofs/map_proof/map_proof_builder.py
+++ exonum/proofs/map_proof/map_proof_builder.py
@@ -8,7 +8,32 @@ from .errors import MapProofBuilderError
 
 
 class MapProofBuilder:
+    """
+    Builder for a MapProof.
+
+    This class is capable of creating a MapProof from the Dict[Any, Any].
+    Since MapProof converts both keys and values to bytes for hashing, this
+    class generates a converter functions from the relevant protobuf structures.
+
+    Designed workflow example:
+
+    ```python
+    proof_builder = MapProofBuilder()
+    proof_builder.set_key_encoder('BitVec', main_module='helpers')
+    proof_builder.set_value_encoder('Wallet', service_name='cryptocurrency', service_module='service')
+    proof = proof_builder.build_proof(map_proof)
+    ```
+
+    If your key/value type should not be converted to bytes through protobuf, use `MapProof.parse`
+    instead and provide your converter functions by yourself.
+
+    If one of the types should be converted manually, but the other one should use protobuf, you can
+    use `build_encoder_function` method which takes a protobuf message class as an argument and
+    returns a converter function.
+    """
+
     def __init__(self):
+        """ Constructor of the MapProofBuilder. """
         self._map_proof = None
         self._key_encoder = None
         self._value_encoder = None
@@ -48,6 +73,29 @@ class MapProofBuilder:
         service_name: Optional[str] = None,
         service_module: Optional[str] = None
     ) -> 'MapProofBuilder':
+        """
+        Method to set the key encoder.
+
+        If the protobuf structure for the key lies in the main module, you should
+        provide `main_module` argument.
+        Otherwise, you should provide both `service_name` and `service_module`.
+
+        Parameters
+        ----------
+        structure_name: str
+            Name of the protobuf structure to be used in converted function.
+        main_module: Optional[str]
+            Name of the main module
+        service_name: Optional[str]
+            Name of the service.
+        service_module: Optional[str]
+            Name of the service module.
+
+        Raises
+        ------
+        MapProofBuilderError
+            If provided data was incorrect, this exception will be rised.
+        """
         self._key_encoder = self._get_encoder(structure_name, main_module, service_name, service_module)
         return self
 
@@ -58,22 +106,83 @@ class MapProofBuilder:
         service_name: Optional[str] = None,
         service_module: Optional[str] = None
     ) -> 'MapProofBuilder':
+        """
+        Method to set the value encoder.
+
+        If the protobuf structure for the value lies in the main module, you should
+        provide `main_module` argument.
+        Otherwise, you should provide both `service_name` and `service_module`.
+
+        Parameters
+        ----------
+        structure_name: str
+            Name of the protobuf structure to be used in converted function.
+        main_module: Optional[str]
+            Name of the main module
+        service_name: Optional[str]
+            Name of the service.
+        service_module: Optional[str]
+            Name of the service module.
+
+        Raises
+        ------
+        MapProofBuilderError
+            If provided data was incorrect, this exception will be rised.
+        """
         self._value_encoder = self._get_encoder(structure_name, main_module, service_name, service_module)
 
         return self
 
-    def parse_proof(self, proof: Dict[Any, Any]) -> MapProof:
-        def build_encoder_function(encoder_class: type) -> Callable[[Dict[Any, Any]], bytes]:
-            def func(data: Dict[Any, Any]):
-                data_json = json.dumps(data)
-                protobuf_obj = encoder_class()
-                return json_format.Parse(data_json, protobuf_obj, True).SerializeToString()
-            return func
+    @staticmethod
+    def build_encoder_function(encoder_class: type) -> Callable[[Dict[Any, Any]], bytes]:
+        """
+        Static method to create encoder function.
 
+        Normally you won't use this function, but if you need to use MapProof with one custom
+        converter function and one protobuf-based, you can create protobuf converter with this
+        method.
+
+        Parameters
+        ----------
+        encoder_class: type
+            Class of the protobuf message that will be used to convert data to bytes.
+
+        Returns
+        ------
+        Callable[[Dict[Any, Any]], bytes]
+            A converter function for the MapProof.
+        """
+        def func(data: Dict[Any, Any]):
+            data_json = json.dumps(data)
+            protobuf_obj = encoder_class()
+            return json_format.Parse(data_json, protobuf_obj, True).SerializeToString()
+        return func
+
+    def build_proof(self, proof: Dict[Any, Any]) -> MapProof:
+        """
+        Method to build the MapProof from the Dict[Any, Any].
+
+        This method should be called only after `set_key_encoder` and `set_value_encoder`.
+
+        Parameters
+        ----------
+        proof: Dict[Any, Any]
+            Raw ProofMap that will be parsed.
+
+        Raises
+        ------
+        MapProofBuilderError
+            If method is called without prior `set_key_encoder` and `set_value_encoder` calls,
+            this exception will be rised.
+
+        MalformedMapProofError
+            If the provided raw MapProof was malformed and parsing failed, this exception will
+            be rised.
+        """
         if not self._key_encoder or not self._value_encoder:
             raise MapProofBuilderError("Encoders aren't set")
 
-        key_encoder_func = build_encoder_function(self._key_encoder)
-        value_encoder_func = build_encoder_function(self._value_encoder)
+        key_encoder_func = self.build_encoder_function(self._key_encoder)
+        value_encoder_func = self.build_encoder_function(self._value_encoder)
 
         return MapProof.parse(proof, key_encoder_func, value_encoder_func)
