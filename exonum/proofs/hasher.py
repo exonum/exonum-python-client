46015f0c807243aebe4e57fa1674e9a63305c3d0
Igor Aleksanov <popzxc@yandex.ru>
2019-09-03 13:10:52 +0300

    Documentation

--- exonum/proofs/hasher.py
+++ exonum/proofs/hasher.py
@@ -19,46 +19,78 @@ class Hasher:
 
     @staticmethod
     def hash_raw_data(data: bytes) -> bytes:
+        """ SHA256 hash of the provided data. """
+
         return crypto_hash_sha256(data)
 
     @staticmethod
     def hash_node(left: bytes, right: bytes) -> bytes:
+        """ Convenience method to obtain hashed value of the merkle tree node. """
+
         data = struct.pack('<B', Hasher.HashTag.LIST_BRANCH_NODE) + left + right
 
         return crypto_hash_sha256(data)
 
     @staticmethod
     def hash_single_node(left: bytes) -> bytes:
+        """ Convenience method to obtain a hashed value of the merkle tree node with one child. """
+
         data = struct.pack('<B', Hasher.HashTag.LIST_BRANCH_NODE) + left
 
         return crypto_hash_sha256(data)
 
     @staticmethod
     def hash_leaf(val: bytes) -> bytes:
+        """ Convenience method to obtain a hashed value of the merkle tree leaf. """
+
         data = struct.pack('<B', Hasher.HashTag.BLOB) + val
 
         return crypto_hash_sha256(data)
 
     @staticmethod
     def hash_list_node(length: int, merkle_root: bytes) -> bytes:
+        """
+        Hash of the list object.
+        ```text
+        h = sha-256( HashTag::ListNode || len as u64 || merkle_root )
+        ```
+        """
         data = struct.pack('<BQ', Hasher.HashTag.LIST_NODE, length) + merkle_root
 
         return crypto_hash_sha256(data)
 
     @staticmethod
     def hash_map_node(root: bytes) -> bytes:
+        """
+        Hash of the map object.
+        ```text
+        h = sha-256( HashTag::MapNode || merkle_root )
+        ```
+        """
         data = struct.pack('<B', Hasher.HashTag.MAP_NODE) + root
 
         return crypto_hash_sha256(data)
 
     @staticmethod
     def hash_map_branch(branch_node: bytes) -> bytes:
+        """
+        Hash of the map branch node.
+        ```text
+        h = sha-256( HashTag::MapBranchNode || <left_key> || <right_key> || <left_hash> || <right_hash> )
+        ```
+        """
         data = struct.pack('<B', Hasher.HashTag.MAP_BRANCH_NODE) + branch_node
 
         return crypto_hash_sha256(data)
 
     @staticmethod
     def hash_single_entry_map(path, h: bytes) -> bytes:
+        """
+        Hash of the map with single entry.
+        ``` text
+        h = sha-256( HashTag::MapBranchNode || <key> || <child_hash> )
+        ```
+        """
         data = struct.pack('<B', Hasher.HashTag.MAP_BRANCH_NODE) + path.as_bytes() + h
 
         return crypto_hash_sha256(data)
--- exonum/proofs/map_proof/map_proof.py
+++ exonum/proofs/map_proof/map_proof.py
@@ -21,7 +21,7 @@ class MapProofEntry:
 
     @staticmethod
     def parse(data: Dict[str, str]) -> 'MapProofEntry':
-        """ Parses MapProofEntry from the json. """
+        """ Parses MapProofEntry from the provided dict. """
 
         if not isinstance(data.get('path'), str) or not is_field_hash(data, 'hash'):
             raise MalformedMapProofError.malformed_entry(data)
@@ -35,6 +35,19 @@ class MapProofEntry:
 
 
 def collect(entries: List[MapProofEntry]) -> bytes:
+    """
+    Computes the root hash of the Merkle Patricia tree backing the specified entries
+    in the map view.
+    The tree is not restored in full; instead, we add the paths to
+    the tree in their lexicographic order (i.e., according to the `PartialOrd` implementatio
+    of `ProofPath`) and keep track of the rightmost nodes (the right contour) of the tree.
+    It is easy to see that adding paths in the lexicographic order means that only
+    the nodes in the right contour may be updated on each step. Further, on each step
+    zero or more nodes are evicted from the contour, and a single new node is
+    added to it.
+    `entries` are assumed to be sorted by the path in increasing order.
+    """
+
     def common_prefix(x: ProofPath, y: ProofPath) -> ProofPath:
         return x.prefix(x.common_prefix_len(y))
 
@@ -97,6 +110,7 @@ def collect(entries: List[MapProofEntry]) -> bytes:
 
 
 class CheckedMapProof:
+    """ Version of `MapProof` obtained after verification. """
     def __init__(self, entries: List[OptionalEntry], root_hash: bytes):
         self._entries = entries
         self._root_hash = root_hash
@@ -119,6 +133,35 @@ class CheckedMapProof:
 
 
 class MapProof:
+    """
+    Proof of existence for a Map.
+
+    This class is capable of parsing a MapProof from the Dict[Any, Any] and
+    verifying parsed proof.
+
+    In most cases you should not create MapProof manually (use MapProofBuilder instead).
+
+    However, if you understand that MapProofBuilder doesn't suit your needs, here is the designed
+    workflow example:
+
+    ```python
+    proof = get_proof_somehow()
+
+    # Assuming that protobuf files for service already loaded and compiled.
+    cryptocurrency_service_name = 'exonum-cryptocurrency-advanced:0.11.0'
+    cryptocurrency_module = ModuleManager.import_service_module(cryptocurrency_service_name, 'service')
+
+    cryptocurrency_decoder = MapProofBuilder.build_encoder_function(cryptocurrency_module.Wallet)
+
+    # Keys will be encoded to bytes using "bytes.fromhex",
+    # and values will be encoded using "Wallet" protobuf structure.
+    parsed_proof = MapProof.parse(proof, lambda x: bytes.fromhex(x), cryptocurrency_decoder)
+
+    # CheckedMapProof will be returned.
+    result = parsed_proof.check()
+    ```
+    """
+
     def __init__(
         self,
         entries: List[OptionalEntry],
@@ -126,6 +169,9 @@ class MapProof:
         key_to_bytes: Callable[[Any], bytes],
         value_to_bytes: Callable[[Any], bytes]
     ):
+        """
+        Constructor of the MapProof. It shouldn't be called directly, use `MapProof.parse`.
+        """
         self.entries = entries
         self.proof = proof
         self._key_to_bytes = key_to_bytes
@@ -142,6 +188,61 @@ class MapProof:
         key_to_bytes: Callable[[Any], bytes],
         value_to_bytes: Callable[[Any], bytes]
     ) -> 'MapProof':
+        """
+        Method to parse the proof.
+
+        Example of the expected proof format:
+
+        ```python
+        proof = {
+          "entries": [
+            {
+              "key": "e610db75b0bbbd4c606c4f8ca3fca9f916e9c8ae9a93b5b767082172454344b3",
+              "value": {
+                "pub_key": {
+                  "data": list(bytes.fromhex("e610db75b0bbbd4c606c4f8ca3fca9f916e9c8ae9a93b5b767082172454344b3"))
+                },
+                "name": "Alice1",
+                "balance": 95,
+                "history_len": 6,
+                "history_hash": {
+                  "data": list(bytes.fromhex("19faf859d7456907c76f085af5b7a2d7621d992617a349006c07720957d5d49d"))
+                }
+              }
+            }
+          ],
+          "proof": [
+            {
+              "path": "010000100100100010100100110001100111011011011000010011011011111111110110001101001001011010111100"
+                      "001010010011011010001110011001011001010101100100000101010010000001101011100000010011101101001011"
+                      "1110010011011011101001110000111111000000011111100010001011010000",
+              "hash": "dbeab4aa952e2c2cb3dc921aa42c9b508e2e5961cad2463f7203d228abc204c8"
+            }
+          ]
+        }
+        ```
+
+        Parameters
+        ----------
+        data: Dict[str, Any]
+            Name of the protobuf structure to be used in converted function.
+        key_to_bytes: Callable[[Any], bytes]
+            Function that will be used to convert keys to bytes.
+        value_to_bytes: Callable[[Any], bytes]
+            Function that will be used to convert values to bytes.
+
+        Returns
+        -------
+        MapProof
+            If parsing succeed, MapProof object will be returned.
+
+        Raises
+        ------
+        MalformedMapProofError
+            If the provided raw MapProof was malformed and parsing failed, this exception will
+            be rised.
+        """
+
         if data.get('entries') is None or data.get('proof') is None:
             raise MalformedMapProofError.malformed_entry(data)
 
@@ -165,6 +266,21 @@ class MapProof:
                 assert False, "Incomparable keys in proof"
 
     def check(self) -> CheckedMapProof:
+        """
+        Calculates the root hash from the parsed proof.
+
+        Returns
+        -------
+        CheckedMapProof
+            If no errors were occured during the calculation, a CheckedMapProof entity will be returned.
+
+        Raises
+        ------
+        MalformedMapProofError
+            If the provided raw MapProof was malformed and parsing failed, this exception will
+            be rised.
+        """
+
         def kv_to_map_entry(kv: OptionalEntry) -> MapProofEntry:
             key_bytes = self._key_to_bytes(kv.key)
             key_hash = Hasher.hash_raw_data(key_bytes)
