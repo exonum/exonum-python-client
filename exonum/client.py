7ddc877d0c2a8575dc78f9044d62eab474144424
Igor Aleksanov <popzxc@yandex.ru>
2019-09-17 09:38:16 +0300

    Exonum mempool endpoint -> stats endpoint

--- exonum/client.py
+++ exonum/client.py
@@ -420,9 +420,9 @@ class ExonumClient(ProtobufProviderInterface):
         """ Performs a GET request to the healthcheck Exonum endpoint. """
         return _get(self._system_endpoint("healthcheck"))
 
-    def mempool(self) -> requests.Response:
-        """ Performs a GET request to the mempool Exonum endpoint. """
-        return _get(self._system_endpoint("mempool"))
+    def stats(self) -> requests.Response:
+        """ Performs a GET request to the stats Exonum endpoint. """
+        return _get(self._system_endpoint("stats"))
 
     def user_agent(self) -> requests.Response:
         """ Performs a GET request to the user_agent Exonum endpoint. """
