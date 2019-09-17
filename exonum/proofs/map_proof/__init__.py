b1ca25bda379fd50ec035da4a99ecb2eece110c0
Igor Aleksanov <popzxc@yandex.ru>
2019-09-17 12:06:49 +0300

    Add examples

--- exonum/proofs/map_proof/__init__.py
+++ exonum/proofs/map_proof/__init__.py
@@ -1,3 +1,4 @@
 """MapProof module."""
-from .map_proof import MapProof, CheckedMapProof, _MapProofEntry
+from .map_proof import MapProof, CheckedMapProof
+from .map_proof_builder import MapProofBuilder
 from .errors import MalformedMapProofError
